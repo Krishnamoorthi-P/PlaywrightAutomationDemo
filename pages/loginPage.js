29488cbb1141deaac9e96f2e9f58a202d3b920de
Krishnamoorthi-P <moorthi7845@gmail.com>
2026-03-03 09:44:12 +0530

    pom implemented

---
+++ pages/loginPage.js
@@ -0,0 +1,16 @@
+
+import { expect }  from '@playwright/test';
+export class loginPage {
+  constructor(page) {
+    //access the page here
+    this.page = page;
+
+    this.userName = page.l;
+    this.password = password;
+  }
+
+  async login(){
+    
+    
+  }
+}
